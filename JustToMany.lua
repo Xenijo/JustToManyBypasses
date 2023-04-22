c1a86fa2b9417fdef6e8e7057a917ded8c1fb127
Xenijo <80117820+Xenijo@users.noreply.github.com>
2023-04-22 21:27:34 +0200

    Update JustToMany.lua

--- JustToMany.lua
+++ JustToMany.lua
@@ -1,16 +1,4 @@
 local Bypasses = {}
 
 
-function Bypasses:Dahood()
-local OldFunction
-OldFunction = hookfunction(getrawmetatable(game).__namecall, newcclosure(function(self, ...)
-        local Args = {...}
-if not checkcaller() and self.Name == "MainEvent" and Args[1] == "TeleportDetect"then 
-warn("JustToManyBypasses Bypassed: "..Args[1])
-return nil
-    end
-
-    return OldFunction(self, ...)
-end))
- end
 return Bypasses
