66c43443dc84ecd2034310280c3c9f8e31fb73ee
Xenijo <80117820+Xenijo@users.noreply.github.com>
2023-04-28 18:28:24 +0200

    Update JustToMany.lua

--- JustToMany.lua
+++ JustToMany.lua
@@ -9,12 +9,12 @@ local Bypass
 Bypass =  hookmetamethod(game, "__namecall", function(self,...)
         local Args = {...}
 if not checkcaller() and tostring(self.Name) == "MainEvent" and  rawequal(Args[1],"TeleportDetect") then 
-      
-return nil
-    end
-    if _G.Settings.Logs == true then 
+        if _G.Settings.Logs == true then 
           warn(Bypassed: "..Args[1])
           end
+return nil
+    end
+    
     return Bypass(self, ...)
 end)
 
@@ -23,12 +23,12 @@ local Fly
 Fly =  hookmetamethod(game, "__namecall", function(self,...)
         local Args = {...}
 if not checkcaller() and tostring(self.Name) == "MainEvent" and  rawequal(Args[1],"CHECKER_1")  or  rawequal(Args[1],"OneMoreTime")  then 
-
-return nil
-    end
-         if _G.Settings.Logs == true then 
+  if _G.Settings.Logs == true then 
           warn(Bypassed: "..Args[1])
           end
+return nil
+    end
+        
     if (not checkcaller() and getfenv(2).crash) then
         local fenv = getfenv(2)
         fenv.crash = function() end
